9b05610529b713e345e72ef7af57f40849d10511
Erika11 <leguizamon.erika@gmail.com>
2013-06-11 12:55:25 -0300

    Ejercicio 7.

--- app.js
+++ app.js
@@ -27,6 +27,8 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+app.get('/ejercicio7', routes.ejercicio7);
+app.post('/ejercicio7', routes.ejercicio7);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
